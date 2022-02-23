eab007a7f227d5fce000a757a3e6e82de79020c6
Carlos Cantu <carloscq920@gmail.com>
2022-02-23 10:51:55 -0600

    Added an about  route & controller, added Bootstrap 5.1 to the application and added a navbar with modified links that were converted to helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # About path
+  get 'about-us', to: 'about#index', as: :about
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'main#index'
 end
