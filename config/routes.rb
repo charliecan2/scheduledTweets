3754266159e152ef16d68b6ff3f9449a34456108
Carlos Cantu <carloscq920@gmail.com>
2022-02-23 19:06:57 -0600

    Created the sign up page, added the sign up route for both the get and post requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   # About path
-  get 'about-us', to: 'about#index', as: :about
+  get 'about', to: 'about#index', as: :about
+
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
   # Defines the root path route ("/")
   root to: 'main#index'
 end
